413f5e698f8add7657d80fa5a38a1a9c7d4f7850
Tushar Singh <rajputtushar309@gmail.com>
2022-12-29 22:31:34 +0530

    added cors

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const dotenv = require("dotenv");
-const express =require('express');
-const mongoose =require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const cookieParser=require('cookie-parser')
+const cookieParser = require('cookie-parser')
 
-dotenv.config({path:'./config.env'});
+app.use(cors())
+
+dotenv.config({ path: './config.env' });
 
 require('./db/connect')
-const User= require('./models/userschema')
-const Post=require('./models/postschema')
+const User = require('./models/userschema')
+const Post = require('./models/postschema')
 
 
 
@@ -23,9 +25,9 @@ app.use(require('./routes/post'));
 app.use(express.json());
 app.use(require('./routes/follow'));
 
- 
 
-if ( process.env.NODE_ENV == "production"){
+
+if (process.env.NODE_ENV == "production") {
 
     app.use(express.static("client/build"));
     const path = require("path");
@@ -42,6 +44,6 @@ const PORT = process.env.PORT || 5000;
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is running at port no. ${PORT}`);
 })
